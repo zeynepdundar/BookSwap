b40e3c7bc6290949dfd520e85474629f718b908b
zeynepdndr <dundarzey@itu.edu.tr>
2023-01-21 17:00:51 +0300

    Move custom theme to the file

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import { useFonts } from 'expo-font';
-import { extendTheme, NativeBaseProvider, Center, Button, Text } from "native-base";
+import { NativeBaseProvider, Center, Button, Text } from "native-base";
+import { theme } from './src/theme';
 
 export default function App() {
+
   const [fontsLoaded] = useFonts({
     'poppins-black': require('./src/assets/fonts/Poppins-Black.ttf'),
     'poppins-light': require('./src/assets/fonts/Poppins-Light.ttf'),
@@ -13,63 +15,8 @@ export default function App() {
     return null;
   }
 
-  const customTheme = extendTheme({
-    fontSizes: {
-      sm: 14,
-      md: 16,
-      lg: 20,
-      xl: 30,
-      custom: 24,
-      '123x': 20,
-    },
-    colors: {
-      // Add new color
-      primary: {
-        50: '#7F3DFF',
-        100: 'rgba(127, 61, 255, 0.18)',
-        200: '#323232',
-        300: '#7AC1E4',
-        400: '#47A9DA',
-        500: '#0088CC',
-        600: '#007AB8',
-        700: '#006BA1',
-        800: '#005885',
-        900: '#003F5E',
-      },
-      // Redefining only one shade, rest of the color will remain same.
-      amber: {
-        400: '#d97706',
-      },
-      error: {
-        500: 'rgba(226, 40, 40, 0.72)'
-      }
-    },
-    fontConfig: {
-      Poppins: {
-        200: {
-          normal: "poppins-light",
-        },
-
-        400: {
-          normal: "poppins-regular",
-        },
-        500: {
-          normal: "poppins-medium",
-        },
-        600: {
-          normal: "poppins-black",
-        },
-      }
-    },
-    fonts: {
-      heading: "poppins-black",
-      body: "poppins-medium",
-      mono: "poppins-light"
-    },
-  })
-
   return (
-    <NativeBaseProvider theme={customTheme}>
+    <NativeBaseProvider theme={theme}>
       <Center flex="1">
         <Center flex="1" fontFamily="heading">
           <Text color="primary.50" fontSize="lg" display="flex"  >
