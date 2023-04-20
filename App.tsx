0b5f430100abda2feac6c862a1c70d9d8d8e5363
Zeynep Dundar <zdundar@lingaros.com>
2023-04-20 17:07:43 +0300

    Create gender input screen

--- App.tsx
+++ App.tsx
@@ -15,6 +15,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import VerificationCode from "./src/screens/auth/VerificationCode";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Welcome from './src/screens/WelcomeScreen';
+import GenderInputScreen from './src/screens/GenderInputScreen';
 
 Amplify.configure(awsconfig);
 
@@ -97,11 +98,10 @@ export default function App() {
     return (
       <Stack.Navigator
         screenOptions={{
-          contentStyle: { backgroundColor: "red" },
           headerShown: false,
         }}
       >
-        <Stack.Screen name="NameInput" component={NameInputScreen}></Stack.Screen>
+        <Stack.Screen name="GenderInput" component={GenderInputScreen}></Stack.Screen>
       </Stack.Navigator>
     );
   }
---
+++ src/screens/GenderInputScreen.tsx
@@ -0,0 +1,37 @@
+import { Button, Center, Heading, Input, Text, View } from "native-base";
+import { useState } from "react";
+import Screen from "../components/Screen";
+
+export default function GenderInputScreen() {
+  const [name, setName] = useState<string>("");
+
+  const pressHandler = (event: any) => {
+    console.log("enteredName");
+  };
+
+  return (
+    <Screen>
+      <Heading mt="100px">I am a</Heading>
+      <Center mt="50">
+        <Input 
+          value={name}
+          variant="underlined"
+          maxLength={25}
+          width={330}
+          fontSize={20}
+          borderTopColor="none"
+          borderBottomColor="black.300"
+          color="black.300"
+          textAlign="center"
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <Center mt={100}>
+          <Button variant="primary" onPress={pressHandler}>
+            Continue
+          </Button>
+        </Center>
+      </Center>
+    </Screen>
+  );
+}
