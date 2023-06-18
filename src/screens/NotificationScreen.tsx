6ace6167742aeec6c3c682517b0fe814a9b2c2dd
Zeynep Dundar <zdundar@lingaros.com>
2023-06-18 22:42:47 +0300

    Create bottomtab bar

---
+++ src/screens/NotificationScreen.tsx
@@ -0,0 +1,13 @@
+import { Text } from "native-base";
+import i18n from "../i18n";
+import Screen from "../components/Screen";
+
+export default function NotificationScreen({ navigation }) {
+
+
+  return (
+    <Screen>
+      <Text>Notification Screen</Text>
+    </Screen>
+  );
+}
