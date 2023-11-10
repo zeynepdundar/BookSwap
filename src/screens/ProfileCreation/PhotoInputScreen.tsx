946de4e3f3ff6ccf3f08efc7b9f2defdd48da32e
Zeynep Dundar <zdundar@lingaros.com>
2023-11-10 20:11:57 +0300

    Implement photo upload screen

--- src/screens/ProfileCreation/PhotoInputScreen.tsx
+++ src/screens/ProfileCreation/PhotoInputScreen.tsx
@@ -1,24 +1,93 @@
+import { useState } from "react";
 import {
+  Actionsheet,
+  AddIcon,
   ArrowBackIcon,
-  Box,
+  Badge,
   Button,
   Center,
-  Fab,
   Flex,
   Heading,
-  Icon,
-  Input,
+  Image,
   Pressable,
   Spacer,
-  Text,
-  View,
   VStack,
 } from "native-base";
-import ImagePicker from "../../components/ImagePicker";
+import {
+  launchCameraAsync,
+  launchImageLibraryAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
 import Screen from "../../components/Screen";
 import i18n from "../../i18n";
 
 export default function PhotoInputScreen({ navigation }) {
+  const avatarImage = require("../../assets/images/avatar.png");
+
+  const [pickedImage, setPickedImage] = useState();
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+
+  async function verifyPermissions() {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
+    }
+
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permissions",
+        "You need to grant camera permissions to use this app."
+      );
+      return false;
+    }
+
+    return true;
+  }
+
+  const onClose = () => {
+    setIsOpen(false);
+  };
+
+  async function takeImageHandler() {
+    setIsOpen(false);
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    });
+    setPickedImage(image.assets[0].uri);
+  }
+
+  async function uploadImageHandler() {
+    setIsOpen(false);
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
+    let result = await launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setPickedImage(result.assets[0].uri);
+    }
+    console.log(result);
+  }
+
   const pressHandler = () => {
     navigation.navigate("Wishlist");
   };
@@ -42,32 +111,66 @@ export default function PhotoInputScreen({ navigation }) {
         <Heading w="100%" h="8" px={6}>
           {i18n.t("my-photo")}
         </Heading>
-        <Center w="100%" h="20" px={8}>
-          {/* <Input
-            value={name}
-            variant="underlined"
-            maxLength={25}
-            width={250}
-            fontSize={20}
-            borderTopColor="none"
-            borderBottomColor="black.400"
-            color="black.400"
-            textAlign="center"
-            autoCapitalize="none"
-            autoCorrect={false}
-            onChangeText={(enteredName) => {
-              setName(enteredName);
-            }}
-          /> */}
+        <Center
+          w="100%"
+          h="40%"
+          px={8}
+          justifyItems="center"
+          alignItems="center"
+          backgroundColor="red.100"
+        >
           <Pressable
-            width={"300px"}
-            height="100px"
-            background={"amber.500"}
-            borderColor={"amber.100"}
+            onPress={() => setIsOpen(true)}
+            width="40%"
+            height="120px"
+            borderRadius="full"
           >
-            <Box borderColor="blue.100">huhu</Box>
+            {!pickedImage && (
+              <Image
+                width="100%"
+                height="100%"
+                borderRadius="full"
+                source={avatarImage}
+                alt="image base"
+              />
+            )}
+            {pickedImage && (
+              <Image
+                width="100%"
+                height="100%"
+                borderWidth="5"
+                borderRadius="full"
+                borderColor="#545454"
+                source={{ uri: pickedImage }}
+                alt="image base"
+              />
+            )}
+            <Badge
+              rounded="100"
+              w="7"
+              h="7"
+              bg="#F2F2F2"
+              position="absolute"
+              right={0}
+              bottom={0}
+            >
+              <AddIcon   color="#545454" />
+            </Badge>
           </Pressable>
-          {/* <ImagePicker /> */}
+        </Center>
+        <Image source={{ uri: pickedImage }} alt="image base" />
+        <Center>
+          <Actionsheet isOpen={isOpen} onClose={onClose}>
+            <Actionsheet.Content>
+              <Actionsheet.Item onPress={uploadImageHandler}>
+              {i18n.t("upload-photo")}
+              </Actionsheet.Item>
+              <Actionsheet.Item onPress={takeImageHandler}>
+              {i18n.t("take-photo")}  
+              </Actionsheet.Item>
+              <Actionsheet.Item onPress={onClose}>Cancel</Actionsheet.Item>
+            </Actionsheet.Content>
+          </Actionsheet>
         </Center>
         <Spacer />
         <Center p={4}>
